5e518f46ecbee5376f78ea314b69a6325c292177
Leonardo Santos <leonardo.ledesma2005@gmail.com>
2026-05-21 13:36:28 -0300

    Primeiro commit do projeto

---
+++ Analisador-De-Sentimentos.py
@@ -0,0 +1,12 @@
+""" 
+Introdução a Engenharia de Software. Profº Drº Daniela Bagatini.
+Analisador De Sentimentos realizado como projeto para o 1º semestre de Engenharia de Software.
+O programa tem como objetivo analisar frases e textos e identificar se são NEGATIVAS, NEUTRAS ou POSITIVAS.
+Autores: 
+
+Erica Moraes Balinhas
+Leonardo Ledesma dos Santos
+Leonardo 
+Lúcio 
+
+"""
